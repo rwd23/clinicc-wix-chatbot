e4fd36591324ed68f32f10c528b9dd34f0a1de71
Ross Dunbar <rwd23@me.com>
2026-04-02 17:53:24 +0100

    Force widget page to bypass stale cache

--- app/widget/page.tsx
+++ app/widget/page.tsx
@@ -1,5 +1,8 @@
 import { ChatWidget } from "@/components/chat-widget";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function WidgetPage() {
   return (
     <main className="widget-page">
